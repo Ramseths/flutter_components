13e86aa68517e8d595a8b600a1db3512eedb5527
Ramseths <juan.ramseths@gmail.com>
2021-07-17 20:42:12 -0500

    Agregando lista de rutas pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
  
@@ -10,6 +12,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Componentes APP',
       home: HomePage(),
+      initialRoute: 'home',
+      routes: <String, WidgetBuilder>{
+        'home'      :(BuildContext context) => HomePage(),
+        'alert'  :(BuildContext context) => AlertPage(),
+        'avatar' :(BuildContext context) => AvatarPage(),
+      },
     );
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -43,15 +43,13 @@ class HomePage  extends StatelessWidget {
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
         onTap: (){
-          final route = MaterialPageRoute(
-            builder: (context){
-              return AlertPage();
-            }
-          );
-          Navigator.push(context, route);
+          // final route = MaterialPageRoute(
+          //   builder: (context) => AlertPage(),
+          // );
+          // Navigator.push(context, route);
+          Navigator.pushNamed(context, opt['ruta']);
         },
         );
-
       opciones..add(widgetTemp)
               ..add(Divider());
     });
