72c4e4e65338989b0965c52e5cf06410fba6363b
Ramseths <juan.ramseths@gmail.com>
2021-07-17 21:06:53 -0500

    Creando archivo routes.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
-import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -11,12 +10,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Componentes APP',
-      home: HomePage(),
       initialRoute: 'home',
-      routes: <String, WidgetBuilder>{
-        'home'      :(BuildContext context) => HomePage(),
-        'alert'  :(BuildContext context) => AlertPage(),
-        'avatar' :(BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (settings){
+        print('Ruta no encontrada ${settings.name}');
+        return MaterialPageRoute(
+          builder: (BuildContext context) => AlertPage()
+        );
       },
     );
   }
