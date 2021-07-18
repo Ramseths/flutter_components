c52e76d93ccb36d506a3be83d76b7cea47c3f1f9
Ramseths <juan.ramseths@gmail.com>
2021-07-17 21:51:40 -0500

    Creando el card 1

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -8,6 +8,39 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cards'),
       ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1(),
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue),
+            title: Text('Soy el titulo de esta tarjeta'),
+            subtitle: Text('Aquí sería la descripción de la tarjeta'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                child: Text('CANCELAR'),
+                onPressed: (){},
+              ),
+              TextButton(
+                child: Text('OK'),
+                onPressed: (){},
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
