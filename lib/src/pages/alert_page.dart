7491817bb48cc00bb74dd81849e7bd2813ec1c85
Ramseths <juan.ramseths@gmail.com>
2021-07-20 12:17:23 -0500

    Creando widget tipo alerta

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,12 +9,51 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _mostrarAlerta(context),
+          child: Text('Mostrar alerta'),
+          style: ElevatedButton.styleFrom(
+            shape: StadiumBorder(),
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.logout),
         onPressed: (){
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _mostrarAlerta(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Este es el contenido de la alerta'),
+              FlutterLogo(size: 100,)
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(), 
+              child: Text('CANCELAR')),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              }, 
+              child: Text('OK'))
+          ],
+        );
+      }
     );
   }
 }
