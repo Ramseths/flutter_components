f4716e0f7f5feaaebd1614981cf5ff41c5148aef
Ramseths <juan.ramseths@gmail.com>
2021-07-22 18:55:26 -0500

    Creando inputs para email y password

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -9,6 +9,8 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
 
   String _nombre = '';
+  String _email = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +21,10 @@ class _InputPageState extends State<InputPage> {
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         children: [
           _crearInput(),
+          Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
           Divider(),
           _crearPersona()
         ],
@@ -53,6 +59,41 @@ class _InputPageState extends State<InputPage> {
   Widget _crearPersona(){
     return ListTile(
       title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email: $_email'),
+    );
+  }
+
+  Widget _crearEmail(){
+    return TextField(
+      //autofocus: true,
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        hintText: 'Email',
+        labelText: 'Email',
+        suffixIcon: Icon(Icons.alternate_email),
+        icon: Icon(Icons.email)
+      ),
+      onChanged: (valor) => setState(() {
+          _email = valor;
+      })
+    );
+  }
+
+  Widget _crearPassword(){
+    return TextField(
+      //autofocus: true,
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        hintText: 'Password',
+        labelText: 'Password',
+        suffixIcon: Icon(Icons.lock_open),
+        icon: Icon(Icons.lock)
+      ),
+      onChanged: (valor) => setState(() {
+          _email = valor;
+      })
     );
   }
 
