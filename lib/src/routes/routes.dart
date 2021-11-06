756c50bce76d76553799f500fb3521d2690c51da
Ramseths <juan.ramseths@gmail.com>
2021-11-06 17:44:28 -0600

    Components Update

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,6 +4,8 @@ import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/listview_page.dart';
+import 'package:componentes/src/pages/slider_page.dart';
 import 'package:flutter/material.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
@@ -13,7 +15,9 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
     'avatar' :(BuildContext context) => AvatarPage(),
     'card'   :(BuildContext context) => CardPage(),
     'animatedContainer' : (BuildContext context) => AnimatedContainerPage(),
-    'inputs' : (BuildContext context) => InputPage()
+    'inputs' : (BuildContext context) => InputPage(),
+    'slider' : (BuildContext context) => SliderPage(),
+    'list' :  (BuildContext context) => ListaPage(),
   };
 }
 
